afdb36224e7a5303fbe701c37e1a3635535fbd85
iaia <iaia72160@gmail.com>
2017-03-12 12:01:22 +0900

    don't use dotenv on production

--- config/application.rb
+++ config/application.rb
@@ -6,15 +6,17 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+if not Rails.env.production?
+    Dotenv::Railtie.load
+end
 
 module Src
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-      config.time_zone = "Tokyo"
-      config.beginning_of_week = :sunday
-      config.i18n.default_locale = :ja
-  end
+    class Application < Rails::Application
+        # Settings in config/environments/* take precedence over those specified here.
+        # Application configuration should go into files in config/initializers
+        # -- all .rb files in that directory are automatically loaded.
+        config.time_zone = "Tokyo"
+        config.beginning_of_week = :sunday
+        config.i18n.default_locale = :ja
+    end
 end
