4b7c35ead0d7eccd0af4809821b9811f0b2ed27a
iaia <iaia72160@gmail.com>
2017-05-18 14:11:02 +0000

    tweetの文言修正

--- lib/assets/tweet.rb
+++ lib/assets/tweet.rb
@@ -8,7 +8,7 @@ module Tweet
         config.access_token        = credentials["token"]
         config.access_token_secret = credentials["secret"]
       end
-      client.update("#{watched_tv.tv_program.title_ja}を見ました https://afr-load.herokuapp.com/")
+      client.update("#{watched_tv.tv_program.title_ja}を見ました #午後ロー via: https://afr-load.herokuapp.com/ ")
     rescue Exception => ex
       puts "failed to tweet... #{ex.message}"
     end
