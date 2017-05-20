a7dbf9cafc4da6d6d2e14d8d4d083ad9a86bc159
iaia <iaia72160@gmail.com>
2017-05-20 15:56:00 +0000

    add country views

---
+++ app/policies/country_policy.rb
@@ -0,0 +1,31 @@
+class CountryPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    can?(__method__)
+  end
+
+  def create?
+    can?(__method__)
+  end
+
+  def edit?
+    can?(__method__)
+  end
+
+  def update?
+    can?(__method__)
+  end
+end
