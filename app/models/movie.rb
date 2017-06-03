ce0ba9c98b1ea9667296c424ce840e47b4a8b091
iaia <iaia72160@gmail.com>
2017-06-03 03:26:07 +0000

    slimを修正

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
   has_many :actors, -> { distinct }, through: :actor_movie
   belongs_to :director
   has_many :countries, -> { distinct }, class_name: "MovieCountry"
-  has_many :tv_programs
+  has_many :tv_programs, dependent: :delete_all
   has_one :watched, class_name: "WatchedMovie"
   has_many :thumbnails, -> { distinct }, class_name: "MovieThumbnail"
 
